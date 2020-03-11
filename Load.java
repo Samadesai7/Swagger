50588edcf96102d14c3a18d68a29d7e9ef29a150
Samadesai7 <60087937+Samadesai7@users.noreply.github.com>
2020-03-11 05:24:33 -0700

    Add files via upload
    enumeration and threads

---
+++ Load.java
@@ -0,0 +1,16 @@
+class Some{
+	void work(int val){
+		System.out.println("int");
+	}
+
+	// void work(){
+		
+	// }
+}
+
+
+class OverloadRevisit{
+	public static void main(String[] args) {
+		
+	}
+}
